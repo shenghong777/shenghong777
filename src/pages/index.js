79caca05703a60863ac43ddefab309b8a1e492a7
shenghong777 <53395152+shenghong777@users.noreply.github.com>
2019-08-06 03:37:56 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ const IndexPage = () => (
   <div>
     <h1>Welcome to my Domain</h1>
     <p>Bow down to my Awesomeness!!</p>
-    <Link to="/page-2/"
-    style={{
-      color: "purple",
-      textDecoration: "underline"}}> Go to next page </Link>
+   // <Link to="/page-2/"
+    //style={{
+      //color: "purple",
+      //textDecoration: "underline"}}> Go to next page </Link>
   </div>
 )
 
